6f737a41d44c41211685fb5244e2b29a6d74a654
Tamy <tamy.vivas@gmail.com>
2021-07-21 17:41:37 -0600

    added react apollo hooks

---
+++ graphql-chat/client/src/hook.js
@@ -0,0 +1,30 @@
+import { useMutation, useQuery, useSubscription } from '@apollo/react-hooks';
+
+import {
+    addMessageMutation,
+    messageAddedSubscription,
+    messagesQuery,
+} from './graphql/queries';
+
+export function useChatMessages() {
+    //Custom Hook only extract the logic from Chat component
+    const { data } = useQuery(messagesQuery);
+    const messages = data ? data.messages : [];
+    useSubscription(messageAddedSubscription, {
+        onSubscriptionData: ({ client, subscriptionData }) => {
+            client.writeData({
+                data: {
+                    messages: messages.concat(
+                        subscriptionData.data.messageAdded
+                    ),
+                },
+            });
+        },
+    });
+
+    const [addMessage] = useMutation(addMessageMutation);
+    return {
+        messages,
+        addMessage: (text) => addMessage({ variables: { input: { text } } }),
+    };
+}
